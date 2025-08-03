34fd4fe32ee4b09a290bf8f920502c308c244fc6
bantee-sharma <banteesharma9@gmail.com>
2025-08-03 21:20:21 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,26 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph import StateGraph, END, START, add_messages
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated
+from langchain_core.messages import HumanMessage, BaseMessage
 
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
+class ChatbotSchema(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chatnode(state: ChatbotSchema):
+    response = llm.invoke(state["messages"])
+    return {"message":[response]}
+
+grpah = StateGraph(ChatbotSchema)
+
+grpah.add_node("chatbot", chatnode)
+
+grpah.add_edge(START,"chatbot")
+grpah.add_edge("chatbot",END)
+
+workflow = grpah.compile()
+
+print(workflow.invoke({"message":"hi"}))
