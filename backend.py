8c4288a0aef2beab7dd25e76bf33bb4650b0bdee
bantee-sharma <banteesharma9@gmail.com>
2025-08-03 21:59:58 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,17 +1,31 @@
 import streamlit as st
+from chatbot import workflow
+from langchain_core.messages import HumanMessage
 
-with st.chat_message('user'):
-    st.text("Hii")
+config = {"configurable":{"thread_id":"1"}}
 
-with st.chat_message("assistant"):
-    st.text("How can i help you?")
+if 'message_history' not in st.session_state:
+    st.session_state["message_history"] = []
 
-with st.chat_message('user'):
-    st.text("My name is nitish")
 
-user_input = st.chat_input("Type here...")
+
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+# {"role":"user", "content":"hii"}
+# {"role":"assistant", "content":"hello"}
+
+user_input = st.chat_input("Type here")
 
 if user_input:
-    
+    st.session_state["message_history"].append({"role":"user", "content": user_input})
     with st.chat_message("user"):
         st.text(user_input)
+
+
+    response = workflow.invoke({"messages": [HumanMessage(content=user_input)]},config=config)
+    ai_msg = response['messages'][-1].content
+    st.session_state["message_history"].append({"role":"assistant", "content": ai_msg})
+    with st.chat_message("assistant"):
+        st.text(ai_msg)
