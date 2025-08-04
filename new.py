64cf85c17dffa4c3d8ef4cf731fadbd89d3ebda8
bantee-sharma <banteesharma9@gmail.com>
2025-08-04 17:04:44 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from chatbot import workflow
+from langchain_core.messages import HumanMessage
+
+config = {"configurable": {"thread_id":"1"}}
+
+if 'message_history' not in st.session_state:
+    st.session_state["message_history"] = []
 
 
-message_history = []
-
-for msg in message_history:
+for msg in st.session_state["message_history"]:
     with st.chat_message(msg["role"]):
         st.text(msg["content"])
 
@@ -11,10 +16,13 @@ user_input = st.chat_input("Ask me...")
 
 if user_input:
 
-    message_history.append({"role":"user", "content":user_input})
+    st.session_state["message_history"].append({"role":"user", "content":user_input},config=config)
     with st.chat_message("user"):
         st.write(user_input)
 
-    message_history.append({"role":"user", "content":user_input})
+
+    response = workflow.invoke({"messages":HumanMessage(content=user_input)})
+    ai_msg = response["messages"][-1]
+    st.session_state["message_history"].append({"role":"user", "content":ai_msg})
     with st.chat_message("assistant"):
-        st.write(user_input)
+        st.write(ai_msg)
