c8b1c285eb70b230b0e3a18e070def645ac9a048
bantee-sharma <banteesharma9@gmail.com>
2025-08-04 16:53:44 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hii")
 
-with st.chat_message("assistant"):
-    st.write("Hii")
+message_history = []
+
+
+user_input = st.chat_input("Ask me...")
+
+if user_input:
+
+    message_history.append({"role":"user", "content":user_input})
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    message_history.append({"role":"user", "content":user_input})
+    with st.chat_message("assistant"):
+        st.write(user_input)
