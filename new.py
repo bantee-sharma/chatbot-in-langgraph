f0db2c74f27fa4bb97e2b079eab635aea313cd72
bantee-sharma <banteesharma9@gmail.com>
2025-08-06 17:20:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,8 +2,16 @@ import streamlit as st
 from langchain_core.messages import HumanMessage
 from chatbot import HumanMessage
 
-with st.chat_message("user"):
-    st.text("Hii")
 
-with st.chat_message("assistant"):
-    st.text("Hwello")
+
+user_input = st.chat_input("Type here....")
+
+
+if user_input:
+        
+    with st.chat_message("user"):
+        st.text(user_input)
+
+    with st.chat_message("assistant"):
+        st.text(user_input)
+
