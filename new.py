fa4a2fd0075241bc2cded3d7c71c7ec9b97e9e84
bantee-sharma <banteesharma9@gmail.com>
2025-08-06 17:24:05 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,16 +2,22 @@ import streamlit as st
 from langchain_core.messages import HumanMessage
 from chatbot import HumanMessage
 
+if 'message_history' not in st.session_state:
+    st.session_state["message_history"] = []
 
+for msg in st.session_state["message_history"]:
+    with st.chat_message(msg['role']):
+        st.write(msg["content"])
 
 user_input = st.chat_input("Type here....")
 
-
 if user_input:
-        
+
+    st.session_state["message_history"].append({"role":"user", "content":user_input})
     with st.chat_message("user"):
         st.text(user_input)
 
+    st.session_state["message_history"].append({"role":"assistant", "content":user_input})
     with st.chat_message("assistant"):
         st.text(user_input)
 
