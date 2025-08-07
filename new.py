19b166adc81ff926285e36db61b32b57da31befe
bantee-sharma <banteesharma9@gmail.com>
2025-08-07 21:18:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -30,6 +30,6 @@ if user_input:
 
     response = workflow.invoke({"messages": HumanMessage(content=user_input)},config = config)
     ai_msg = response["messages"][-1].content
-    st.session_state["message_history"].append({"role":"user","content": ai_msg})
+    st.session_state["message_history"].append({"role":"assistant","content": ai_msg})
     with st.chat_message("assistant"):
         st.text(ai_msg)
