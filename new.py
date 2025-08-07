f963795e9281cb065a71317d4b07b126370d70dc
bantee-sharma <banteesharma9@gmail.com>
2025-08-07 22:13:53 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,60 +1,27 @@
 import streamlit as st
-from chatbot import workflow
 from langchain_core.messages import HumanMessage
-import uuid
+from chatbot import workflow
 
-def gen_thread_id():
-    thread_id = uuid.uuid4
-    return thread_id
-
-def reset_chat():
-    thread_id = gen_thread_id()
-    st.session_state["thread_id"] = thread_id
-    add_thread(st.session_state["thread_id"])
+if 'message_history' not in st.session_state:
     st.session_state["message_history"] = []
-
-def add_thread(thread_id):
-    if thread_id not in st.session_state["chat_threads"]:
-        st.session_state["chat_threads"].append(thread_id)
-
-if "message_history" not in st.session_state:
-    st.session_state["message_history"] = []
-
-if "thread_id" not in st.session_state:
-    st.session_state["thread_id"] = gen_thread_id()
-
-if "chat_threads" not in st.session_state:
-    st.session_state["chat_threads"] = []
-
-
-add_thread(st.session_state["thread_id"])
-
-config = {"configurable": {"thread_id": st.session_state["thread_id"]}}
 
 for msg in st.session_state["message_history"]:
     with st.chat_message(msg["role"]):
         st.text(msg["content"])
 
-st.sidebar.title("LangGraph Chatbot")
-
-if st.sidebar.button("New Chat"):
-    reset_chat()
-
-st.sidebar.header("My Chats")
-st.sidebar.text(st.session_state["thread_id"])
+config = {"configurable": {"thread_id": "1"}}
 
 user_input = st.chat_input("Type here...")
 
 if user_input:
 
-
-    st.session_state["message_history"].append({"role":"user","content": user_input})
+    st.session_state["message_history"].append(user_input)
     with st.chat_message("user"):
         st.text(user_input)
-    
 
-    response = workflow.invoke({"messages": HumanMessage(content=user_input)},config = config)
+
+    response = workflow.invoke({"messages": HumanMessage(content=user_input)}, config=config)
     ai_msg = response["messages"][-1].content
-    st.session_state["message_history"].append({"role":"assistant","content": ai_msg})
+    st.session_state["message_history"].append(ai_msg)
     with st.chat_message("assistant"):
         st.text(ai_msg)
