14ce3c1f1ded7063c55bcfef1bc9f32cfd30f236
bantee-sharma <banteesharma9@gmail.com>
2025-08-07 21:17:51 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,7 +4,13 @@ from langchain_core.messages import HumanMessage
 
 config = {"configurable": {"thread_id": "1"}}
 
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
 
+
+for msg in st.session_state["message_history"]:
+    with st.chat_message(msg["role"]):
+        st.text(msg["content"])
 
 st.sidebar.title("LangGraph Chatbot")
 
@@ -16,11 +22,14 @@ user_input = st.chat_input("Type here...")
 
 if user_input:
 
+
+    st.session_state["message_history"].append({"role":"user","content": user_input})
     with st.chat_message("user"):
         st.text(user_input)
     
 
     response = workflow.invoke({"messages": HumanMessage(content=user_input)},config = config)
     ai_msg = response["messages"][-1].content
+    st.session_state["message_history"].append({"role":"user","content": ai_msg})
     with st.chat_message("assistant"):
         st.text(ai_msg)
