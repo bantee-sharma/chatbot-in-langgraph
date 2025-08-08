1fc11cf00e914a13007573baf1b6d5eae97e7c65
bantee-sharma <banteesharma9@gmail.com>
2025-08-08 16:36:45 +0530

    Update n.py

--- n.py
+++ n.py
@@ -8,7 +8,9 @@ config = {"configurable": {"thread_id":"1"}}
 if 'message_history' not in st.session_state:
     st.session_state["message_history"] = []
 
-
+for msg in st.session_state["message_history"]:
+    with st.chat_message(msg["role"]):
+        st.text(msg["content"])
 
 user_input = st.chat_input("Type here...")
 
