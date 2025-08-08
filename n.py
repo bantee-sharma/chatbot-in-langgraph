e63ccb82d1a8598441089f0a9ca4201fe50de1a6
bantee-sharma <banteesharma9@gmail.com>
2025-08-08 16:34:50 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,58 +1,29 @@
-from chatbot import workflow
-from langchain_core.messages import HumanMessage
 import streamlit as st
+from langchain_core.messages import HumanMessage
+from chatbot import workflow
 import uuid
 
-# **************************************** utility functions *************************
+config = {"configurable": {"thread_id":"1"}}
 
-def gen_thread_id():
-    thread_id = uuid.uuid4()
-    return thread_id
-
-def reset_chat():
-    thread_id = gen_thread_id()
-    st.session_state["thread_id"] = thread_id
-    st.session_state["message_history"] = []
-
-# **************************************** Session Setup ******************************
 if 'message_history' not in st.session_state:
     st.session_state["message_history"] = []
 
-if "thread_id" not in st.session_state:
-    st.session_state["thread_id"] = gen_thread_id()
 
 
-# **************************************** Sidebar UI *********************************
-
-st.sidebar.header("LangGraph ChatBot")
-if st.sidebar.button("New Chat"):
-    reset_chat()
-
-st.sidebar.header("My Chats")
-st.sidebar.text(st.session_state["thread_id"])
-
-# **************************************** Main UI ************************************
-
-for msg in st.session_state["message_history"]:
-    with st.chat_message(msg["role"]):
-        st.text(msg["content"])
-
-config = {"configurable": {"thread_id": st.session_state["thread_id"]}}
 user_input = st.chat_input("Type here...")
 
 if user_input:
 
-    st.session_state["message_history"].append({"role":"assistant", "content": user_input})
+    st.session_state["message_history"].append({"role":"user", "content": user_input})
     with st.chat_message("user"):
         st.text(user_input)
 
-
     with st.chat_message("assistant"):
-
+        
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in workflow.stream(
                 {'messages': [HumanMessage(content=user_input)]},
-                config= config,
+                config= {'configurable': {'thread_id': 'thread-1'}},
                 stream_mode= 'messages'
             )
         )
