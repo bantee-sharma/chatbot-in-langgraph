8f4eb7427d7843ff31c7f496c7165a569cec2aa1
bantee-sharma <banteesharma9@gmail.com>
2025-08-10 20:51:12 +0530

    Update f.py

--- f.py
+++ f.py
@@ -2,20 +2,38 @@ from langchain_core.messages import HumanMessage
 from chatbot import workflow
 import streamlit as st
 from chatbot import workflow
+import uuid
 
+# **************************************** utility functions *************************
+def gen_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+# **************************************** session setup *************************
 if "message_history" not in st.session_state:
     st.session_state["message_history"] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state["thread_id"] = gen_thread_id()
 
 for msg in st.session_state["message_history"]:
     with st.chat_message(msg["role"]):
         st.text(msg["content"])
 
+# **************************************** sidebar *************************
 
+st.sidebar.header("LangGraph ChatBot")
+st.sidebar.button("New Chat")
+st.sidebar.header("My Chats")
+st.sidebar.text(st.session_state["thread_id"])
+
+
+# **************************************** main ui *************************
 user_input = st.chat_input("Type here...")
 
 
 
-config = {"configurable":{"thread_id":"1"}}
+config = {"configurable":{"thread_id":st.session_state["thread_id"]}}
 if user_input:
 
 
