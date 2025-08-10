1d2c0472824d3f771cc86a6912e7bee8b3cf141a
bantee-sharma <banteesharma9@gmail.com>
2025-08-10 20:32:08 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,13 +1,19 @@
 from langchain_core.messages import HumanMessage
 from chatbot import workflow
 import streamlit as st
+from chatbot import workflow
 
 user_input = st.chat_input("Type here...")
 
+
+config = {"configurable":{"config":"1"}}
 if user_input:
 
     with st.chat_message("user"):
         st.text(user_input)
 
+
+    response = workflow.invoke({"messages":[HumanMessage(content=user_input)]},config=config)
+    ai_msg = response["messages"][-1].content
     with st.chat_message("assistant"):
-        st.text(user_input)
+        st.text(ai_msg)
