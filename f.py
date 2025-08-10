d9109c562bd972718a24a8602978c8429561b570
bantee-sharma <banteesharma9@gmail.com>
2025-08-10 20:36:19 +0530

    Update f.py

--- f.py
+++ f.py
@@ -3,17 +3,23 @@ from chatbot import workflow
 import streamlit as st
 from chatbot import workflow
 
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
 user_input = st.chat_input("Type here...")
 
 
-config = {"configurable":{"config":"1"}}
+config = {"configurable":{"thread_id":"1"}}
 if user_input:
 
+
+    st.session_state["message_history"].append({"role":"user", "content": user_input})
     with st.chat_message("user"):
         st.text(user_input)
 
-
+    
     response = workflow.invoke({"messages":[HumanMessage(content=user_input)]},config=config)
     ai_msg = response["messages"][-1].content
+    st.session_state["message_history"].append({"role":"assistant", "content": ai_msg})
     with st.chat_message("assistant"):
         st.text(ai_msg)
